c5fd310a677b3961f020a898b473417bc47cecee
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-08 20:08:33 -0500

    change quotes from double to single

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-gray-800 text-white py-4 px-4 text-center">
+    <footer className='fixed bottom-0 w-full bg-gray-800 text-white py-4 px-4 text-center'>
       &copy; {new Date().getFullYear()} Pedro's Next Blog
     </footer>
   );
